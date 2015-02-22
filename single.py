ce05a71ee13e6fd2957fe2be0c52605ebf5b4122
Marcus McCurdy <marcus.mccurdy@gmail.com>
2015-02-21 21:34:01 -0500

    Updates to python3

--- single.py
+++ single.py
@@ -1,35 +1,21 @@
-from functools import partial
-import json
 import logging
 import os
 from pathlib import Path
-import requests
 
-logging.getLogger('requests').setLevel(logging.CRITICAL)
+from imgur.gallery import get_links, download_link
+
+
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-
-def get_links(client_id):
-    headers = {'Authorization': 'Client-ID {}'.format(client_id)}
-    resp = requests.get('https://api.imgur.com/3/gallery/', headers=headers)
-    data = json.loads(resp.content)
-    return map(lambda item: item['link'], data['data'])
-
-
-def download_link(directory, link):
-    logging.info('Downloading %s', link)
-    image = requests.get(link)
-    download_path = directory / os.path.basename(link)
-    with download_path.open('wb') as f:
-        f.write(image.content)
+logging.getLogger('requests').setLevel(logging.CRITICAL)
 
 
 def main(client_id):
     download_dir = Path('images')
     if not download_dir.exists():
         download_dir.mkdir()
-    links = get_links(client_id)
-    map(partial(download_link, download_dir), links[:10])
+    links = [l for l in get_links(client_id) if l.endswith(('.gif', '.jpg'))]
+    for link in links[:10]:
+        download_link(download_dir, link)
 
 
 if __name__ == '__main__':
