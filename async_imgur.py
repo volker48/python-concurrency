1f02786eb31e3d5edb495a78a2b5b4b811cf962b
Marcus McCurdy <marcus.mccurdy@gmail.com>
2015-02-23 09:18:37 -0500

    Looks like async lib doesn't just work with urllib

--- async_imgur.py
+++ async_imgur.py
@@ -2,12 +2,14 @@ import json
 import logging
 import os
 from pathlib import Path
+import asyncio
 from time import time
 from urllib.request import Request, urlopen
 
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logging.getLogger('requests').setLevel(logging.CRITICAL)
+
 logger = logging.getLogger(__name__)
 
 
@@ -19,6 +21,7 @@ def get_links(client_id):
     return map(lambda item: item['link'], data['data'])
 
 
+@asyncio.coroutine
 def download_link(directory, link):
     logger.info('Downloading %s', link)
     download_path = directory / os.path.basename(link)
@@ -37,14 +40,14 @@ def main(client_id):
     ts = time()
     download_dir = setup_download_dir()
     links = [l for l in get_links(client_id) if l.endswith(('.gif', '.jpg'))]
-    for link in links[:20]:
-        download_link(download_dir, link)
-    print('Took {}s'.format(time() - ts))
-
+    tasks = [asyncio.async(download_link(download_dir, link)) for link in links[:20]]
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
+    print('Took {}'.format(time() - ts))
 
 if __name__ == '__main__':
     client_id = os.getenv('IMGUR_CLIENT_ID')
     if not client_id:
         raise Exception("Couldn't find IMGUR_CLIENT_ID environment variable!")
     main(client_id)
-
