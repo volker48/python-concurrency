5f5b61a6d5916d08652d4311f696978760253d8c
Marcus McCurdy <marcus.mccurdy@gmail.com>
2015-03-01 11:01:40 -0500

    Adds RQ example

---
+++ download.py
@@ -0,0 +1,30 @@
+import json
+import logging
+import os
+from pathlib import Path
+from urllib.request import urlopen, Request
+
+logger = logging.getLogger(__name__)
+
+
+def get_links(client_id):
+    headers = {'Authorization': 'Client-ID {}'.format(client_id)}
+    req = Request('https://api.imgur.com/3/gallery/', headers=headers, method='GET')
+    with urlopen(req) as resp:
+        data = json.loads(resp.readall().decode('utf-8'))
+    return map(lambda item: item['link'], data['data'])
+
+
+def download_link(directory, link):
+    logger.info('Downloading %s', link)
+    download_path = directory / os.path.basename(link)
+    with urlopen(link) as image, download_path.open('wb') as f:
+        f.write(image.readall())
+
+
+def setup_download_dir():
+    download_dir = Path('images')
+    if not download_dir.exists():
+        download_dir.mkdir()
+    return download_dir
+
---
+++ rq_imgur.py
@@ -0,0 +1,29 @@
+import logging
+import os
+from time import time
+
+from redis import Redis
+from rq import Queue
+from download import setup_download_dir, get_links, download_link
+
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.getLogger('requests').setLevel(logging.CRITICAL)
+logger = logging.getLogger(__name__)
+
+
+def main():
+    ts = time()
+    client_id = os.getenv('IMGUR_CLIENT_ID')
+    if not client_id:
+        raise Exception("Couldn't find IMGUR_CLIENT_ID environment variable!")
+    download_dir = setup_download_dir()
+    links = [l for l in get_links(client_id) if l.endswith('.jpg')]
+    q = Queue(connection=Redis(host='localhost', port=6379))
+    for link in links:
+        q.enqueue(download_link, download_dir, link)
+    print('Took {}s'.format(time() - ts))
+
+
+if __name__ == '__main__':
+    main()
